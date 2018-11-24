8d89bf4c20ae06f7b93578b95ba96e12c429626e
hongjun.yue <hongjun.yue@asiainnovations.com>
2018-11-23 20:40:27 -0800

    commit

---
+++ impl/src/main/java/com/asiainno/uplive/push/dao/UserInfoPushDao.java
@@ -0,0 +1,45 @@
+package com.asiainno.uplive.push.dao;
+
+import com.asiainno.uplive.push.api.model.UserInfoPush;
+import com.asiainno.uplive.push.base.BatisEntityDao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserInfoPushDao extends BatisEntityDao {
+
+
+    //根据类型，批量查询
+    public List<UserInfoPush> batchGetPushInfo() {
+        return getSqlSession().selectList("userInfoPush.selectByAudienceCountryCode");
+    }
+    
+   /**
+    * 根据国家批量获取用户信息
+    * yhj
+    * @return
+    */
+    public List<UserInfoPush> batchGetUserInfos() {
+        return getSqlSession().selectList("userInfoPush.selectUserInfoByCountryCode");
+    }
+    
+    public List<UserInfoPush> batchGetUserInfos(Integer feature, String countryCode, List pushUserTypes, Integer start, Integer num) {
+    	List<UserInfoPush> userPushInfos =null;
+    	Map<String, Object> con = new HashMap<>();
+        con.put("countryCode", countryCode);
+        con.put("pushUserType", pushUserTypes);
+        con.put("feature", feature);
+        con.put("start", start);
+        con.put("limit", num);
+        //设置dbKey
+//        DBContextHolder.setDBKey(Long.valueOf(dbKey));
+        userPushInfos = getSqlSession().selectList("userInfoPush.selectUserInfoByCountryCode", con);
+        return userPushInfos;
+    }
+    
+}
+
--- impl/src/main/java/com/asiainno/uplive/push/service/UserPushService.java
+++ impl/src/main/java/com/asiainno/uplive/push/service/UserPushService.java
@@ -2,30 +2,23 @@ package com.asiainno.uplive.push.service;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.asiainno.uplive.push.api.model.UserInfoPush;
 import com.asiainno.uplive.push.api.service.IUserPushService;
-import com.asiainno.uplive.push.common.EnumConstant;
-import com.asiainno.uplive.push.dao.UserPushInfoDao;
-import com.asiainno.uplive.push.utils.FeatureUtils;
-import com.asiainno.uplive.user.service.IFeatureService;
+import com.asiainno.uplive.push.dao.UserInfoPushDao;
 
 @Service
 public class UserPushService implements IUserPushService {
 
     @Autowired
-    private UserPushInfoDao userPushInfoDao;
+    private UserInfoPushDao userInfoPushDao;
     
-    @Autowired
-    private IFeatureService featureService;
 
     @Override
     public List<UserInfoPush> getUserPushInfos(String country) {
-        return userPushInfoDao.batchGetPushInfo();
+        return userInfoPushDao.batchGetPushInfo();
     }
     
     /**
@@ -35,23 +28,11 @@ public class UserPushService implements IUserPushService {
      */
     @Override
     public List<UserInfoPush> getUserPushInfos(String country, Integer feature, String flag, Integer start, Integer num) {
-        List<Integer> inclusiveFeatureList = null;
-        List<Integer> exclusiveFeatureList = null;
-        if (feature != null && !featureService.isUpLive(feature)) {
-            inclusiveFeatureList = getFeatureList(feature);
-        } else {
-            exclusiveFeatureList = getFeatureList(EnumConstant.FEATURE_UPVOICE_MAIN);
-        }
         List flags = null;
         if(flag!= null){
         	flags = Arrays.asList(flag.split(","));
         }
-        
-        return userPushInfoDao.batchGetUserInfos(inclusiveFeatureList, exclusiveFeatureList, country, flags, start, num);
+        return userInfoPushDao.batchGetUserInfos(feature, country, flags, start, num);
     }
 
-    private List<Integer> getFeatureList(Integer feature) {
-        Map<Integer, List<Integer>> appAndFeatureMap = featureService.getAppAndFeatureMap();
-        return FeatureUtils.getFeatureList(feature, appAndFeatureMap);
-    }
 }
